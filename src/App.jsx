abcff6767fe4c272ec0bd6d26a9aa3e9d5b07d1c
Sayem <sayem465508@gmail.com>
2024-03-11 06:36:26 +0600

    set up the environment for rest countries api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>React World Tour</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
